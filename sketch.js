b7f45d813d2dcfb1ff74c1c1df35b7b07203b3cd
Jasper <u5949247@anu.edu.au>
2023-10-21 00:43:19 +1100

    Basic peer 2 peer working

--- sketch.js
+++ sketch.js
@@ -1,25 +1,75 @@
 
-function preload(){
 
+
+
+
+function setup() {
+
+  var peer = new Peer();
+
+
+
+  var peerID = getPeerIDFromURL();
+
+  // If there's a peer ID in the URL, skip the menu and connect to Player 1
+  if (peerID) {
+
+    peer.on('open', function (id) {
+      console.log('My peer ID is: ' + id);
+      var conn = peer.connect(peerID);
+      console.log("Attempting to connect to opponent...");
+
+
+      conn.on('open', function () {
+
+        // Receive messages
+        conn.on('data', function (data) {
+          console.log('Received', data);
+        });
+
+        // Send messages
+        conn.send('Hello from Player 2!');
+      });
+
+
+    });
+
+
+
+
+
+  } else {
+
+
+    peer.on('open', function (id) {
+      console.log('My peer ID is: ' + id);
+      var gameLink = `${window.location.href}?peerID=${id}`;
+      console.log("Share this link with your opponent:", gameLink);
+
+    });
+
+    peer.on('connection', function (conn) {
+      console.log("Connected to opponent!");
+
+
+      conn.on('open', function () {
+        // Receive messages
+        conn.on('data', function (data) {
+          console.log('Received', data);
+        });
+
+        // Send Messages
+        conn.send('Hello from Player 1!');
+
+      });
+
+
+      
+    });
+  }
 }
 
-function setup(){
-
+function getPeerIDFromURL() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get('peerID');
 }
-
-function draw(){
-  
-}
-
-
-
-// Utility funcs
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
-function touchStarted() {
-  mouseClicked();
-}
-
