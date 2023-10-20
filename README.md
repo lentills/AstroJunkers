3582de8a40509c3bf07ec600efd3f0049ad55542
Jasper <u5949247@anu.edu.au>
2023-10-20 23:52:44 +1100

    Uploaded initial files

---
+++ README.md
@@ -0,0 +1,2 @@
+# EXO-JUNKERS
+A web-based p2p space racing game built in p5!
---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+function preload(){
+
+}
+
+function setup(){
+
+}
+
+function draw(){
+  
+}
+
+
+
+// Utility funcs
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function touchStarted() {
+  mouseClicked();
+}
+
